8dc744e017e5ba271716d6f3c6ed96fa77df39ba
minimair <manfred@minimair.org>
2018-01-15 16:12:37 +0000

    added Apache license

--- netdata/workers/paths.py
+++ netdata/workers/paths.py
@@ -1,16 +1,27 @@
 # encoding: utf-8
 """Path utility functions."""
 
-# Copyright (c) 2016, Manfred Minimair
-# Distributed under the terms of the Modified BSD License.
+# Copyright (c) 2016-present, Manfred Minimair
+# All rights reserved.
 
-# Derived from jupyter_core.paths, which is
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Derived from IPython.utils.path, which is
-# Copyright (c) IPython Development Team.
-# Distributed under the terms of the Modified BSD License.
+# This file is derived from jupyter_core.paths, which is
+# copyright (c) Jupyter Development Team
+# and distributed under the terms of the Modified BSD License,
+# and derived from IPython.utils.path, which is
+# copyright (c) IPython Development Team
+# and distributed under the terms of the Modified BSD License.
 
 
 import os
